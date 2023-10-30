ea97c31f35c969e1d5b04275ab368d5b988902a3
jihoo2002 <yolohoo@naver.com>
2023-10-30 15:49:41 +0900

    react 초기 설정완료 및 component 개념 확인

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import expenseItem from './components/expenseItem';
+import NoName from './NoName';
 
 function App() {
+  const $h2 = <h2>반가워용</h2>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <expenseItem />
+      <NoName />
+      <div className='App'>
+        <h1>헬로헬로</h1>
+        {$h2}
+      </div>
+      <div className='noname'>
+        <input type='text' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          오늘은 월요일 입니다...
+          <br />
+          그래서 공부가 하기 싫어요..;;;
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </> //두개 이상 태그를 리턴할 때는 반드시 부모태그가 있어야 한다.
+    //만약 쓸데없는 부모 태그 만들지 않고 싶을 때는 - >React.Fragment사용(생략할 수 있음)
   );
 }
 
-export default App;
+export default App; //index.js에게 app을 넘김
+//react는 <input />등 닫는 태그를 명확하게 표시 해줘야 함
--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//확장자 .js가 숨겨져있음
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />); //roote에서 app.js를 랜더링 하겠다
