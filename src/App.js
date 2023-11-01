e86494516742d31428f96290750312770e557e2b
jihoo2002 <yolohoo@naver.com>
2023-11-01 11:10:23 +0900

    2023 11/1 어제푸시못한 거 까지..

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
+import React from 'react';
 import './App.css';
-import expenseItem from './components/expenseItem';
-import NoName from './NoName';
+import Expenses from './components/Expenses';
+import Hello from './Hello';
 
-function App() {
-  const $h2 = <h2>반가워용</h2>;
+const App = () => {
+  //지출 항목 객체 배열
+
+  const expenses = [
+    {
+      title: '바나나',
+      price: 2000,
+      date: new Date(2023, 3 - 1, 23),
+    },
+    {
+      title: 'BBQ치킨',
+      price: 20000,
+      date: new Date(2023, 5 - 1, 21),
+    },
+    {
+      title: '도미노피자',
+      price: 35000,
+      date: new Date(2023, 7 - 1, 4),
+    },
+  ];
+  console.log('app 실행');
   return (
     <>
-      <expenseItem />
-      <NoName />
-      <div className='App'>
-        <h1>헬로헬로</h1>
-        {$h2}
-      </div>
-      <div className='noname'>
-        <input type='text' />
-        <p>
-          오늘은 월요일 입니다...
-          <br />
-          그래서 공부가 하기 싫어요..;;;
-        </p>
-      </div>
-    </> //두개 이상 태그를 리턴할 때는 반드시 부모태그가 있어야 한다.
-    //만약 쓸데없는 부모 태그 만들지 않고 싶을 때는 - >React.Fragment사용(생략할 수 있음)
+      <Expenses items={expenses} />
+    </>
   );
-}
+};
 
-export default App; //index.js에게 app을 넘김
-//react는 <input />등 닫는 태그를 명확하게 표시 해줘야 함
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-//확장자 .js가 숨겨져있음
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />); //roote에서 app.js를 랜더링 하겠다
+root.render(<App />);
