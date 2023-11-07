95ac080d636db153055fc770a4f2b5e4b248c1ca
jihoo2002 <yolohoo@naver.com>
2023-11-07 11:44:14 +0900

    useEffect 까지

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React from 'react';
-import AddUser from './components/Users/AddUser';
-import UserList from './components/Users/UserList';
+import React, { useEffect, useState } from 'react';
+import MainHeader from './components/sideEffect/MainHeader/MainHeader';
+import Home from './components/sideEffect/Home/Home';
+import Login from './components/sideEffect/Login/Login';
 
 const App = () => {
+  //로그인 상태를 관리하는 변수(초기값 false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //화면이 리렌더링 될 때 localStorage를 확인 해서
+  //현재 login-flag가 존재하는지 검사
+  console.log('로그인 검사 수행');
+
+  //기존에 로그인한 사람인지 확인하는 코드는 리렌더링 될 때마다
+  //실행되면 안됨 userEffect에다 넣으면 리랜더링이 단 한번만 발생하게 할 수 있다.
+  useEffect(() => {
+    console.log('useEffect실행 - 최초 단 한번만 실행');
+    const storedLoginFlag = localStorage.getItem('login-flag');
+    if (storedLoginFlag === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  //서버로 로그인을 요청하는 함수(나중에는 fetch를 통한 백엔드와의 연계가 필요)
+  const loginHandler = (email, password) => {
+    //로그인을 했다는 증거로 상태값 변경 및 브라우저에 로그인 값을 1로 표현해서 저장
+    localStorage.setItem('login-flag', '1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('login-flag');
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div>
-      <AddUser />
-      <UserList />
-    </div>
+    <>
+      <MainHeader
+        isAuthenticated={isLoggedIn}
+        onLogout={logoutHandler}
+      />
+      <main>
+        {isLoggedIn && <Home />}
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+      </main>
+    </>
   );
 };
 
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import './Button.css';
-import styled from 'styled-components';
+import styles from './Button.module.css';
 
-// const Button = styled.button`
-//   font: inherit;
-//   padding: 0.5rem 1.5rem;
-//   border: 1px solid #8b005d;
-//   color: white;
-//   background: #8b005d;
-//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-//   cursor: pointer;
-
-//   &:focus {
-//     outline: none;
-//   }
-
-//   &:hover,
-//   &:active {
-//     background: #ac0e77;
-//     border-color: #ac0e77;
-//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-//   }
-// `;
-
-//타입이 만약 전달이 안될 경우를 대비하여 'button'을 넣음
-const Button = ({ type, onClick, children }) => {
+const Button = ({ type, onClick, children, className, disabled }) => {
   return (
     <button
       type={type || 'button'}
-      className='button'
+      className={`${styles.button} ${className}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
   );
 };
-
 export default Button;
